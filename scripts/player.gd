59913a9bc2b0b2a1d37371ca26ff8cf8170f76a7
Eusébio Alves <eusebio.a95@hotmail.com>
2024-01-07 18:00:53 +0000

    Added horizontal movement and gravity to script

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,3 +1,17 @@
 extends CharacterBody2D
 
+@onready var animated_sprite = $AnimatedSprite2D
 
+var GRAVITY = 400
+var SPEED = 150
+
+func _physics_process(delta):
+	
+	if !is_on_floor():
+		velocity.y += GRAVITY * delta
+		
+	var direction = Input.get_axis('left', 'right')
+	
+	velocity.x = direction * SPEED
+	
+	move_and_slide()
