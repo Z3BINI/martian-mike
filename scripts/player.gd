2f6f6a9d35a3f75975e6d672ecb34d7408cc43a7
Eusébio Alves <eusebio.a95@hotmail.com>
2024-01-07 18:09:11 +0000

    Check if grounded before jump

--- scripts/player.gd
+++ scripts/player.gd
@@ -13,7 +13,7 @@ func _physics_process(delta):
 		if velocity.y == 500:
 			velocity.y = 500
 	
-	if Input.is_action_just_pressed('jump'):
+	if Input.is_action_just_pressed('jump') && is_on_floor():
 		velocity.y = -JUMP_FORCE
 		
 	var direction = Input.get_axis('left', 'right')
