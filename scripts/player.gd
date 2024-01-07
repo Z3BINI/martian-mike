ac9cf871200c12bb8077864c8524e9122508809e
Eusébio Alves <eusebio.a95@hotmail.com>
2024-01-07 18:04:41 +0000

    Cam2D, jump and fall vel cap

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,11 +4,17 @@ extends CharacterBody2D
 
 var GRAVITY = 400
 var SPEED = 150
+var JUMP_FORCE = 200
 
 func _physics_process(delta):
 	
 	if !is_on_floor():
 		velocity.y += GRAVITY * delta
+		if velocity.y == 500:
+			velocity.y = 500
+	
+	if Input.is_action_just_pressed('jump'):
+		velocity.y = -JUMP_FORCE
 		
 	var direction = Input.get_axis('left', 'right')
 	
