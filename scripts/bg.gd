ff2e03ecd3c09927242320bc03e0db9af8ec1ea9
Eusébio Alves <eusebio.a95@hotmail.com>
2024-01-08 12:30:34 +0000

    Parallax background setup

---
+++ scripts/bg.gd
@@ -0,0 +1,16 @@
+extends ParallaxBackground
+
+@export var scroll_speed = 25
+@export var bg_texture : CompressedTexture2D = preload('res://assets/textures/bg/Blue.png')
+
+@onready var sprite = $ParallaxLayer/Sprite2D
+
+func _ready():
+	sprite.texture = bg_texture
+
+func _process(delta):
+	sprite.region_rect.position += Vector2(-scroll_speed, scroll_speed) * delta
+	
+	if sprite.region_rect.position.x <= -32 && sprite.region_rect.position.y >= 32:
+		sprite.region_rect.position = Vector2.ZERO
+	
