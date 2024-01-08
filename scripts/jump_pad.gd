56344c11ea946e046fbd0d5146bb71975edcfe5d
Eusébio Alves <eusebio.a95@hotmail.com>
2024-01-08 09:16:28 +0000

    Created jump logic for pads

--- scripts/jump_pad.gd
+++ scripts/jump_pad.gd
@@ -1,5 +1,9 @@
 extends Area2D
 
+@onready var animated_sprite = $AnimatedSprite2D
+
+@export var JUMP_FORCE = 300
 
 func _on_body_entered(body):
-	print('jump')
+	animated_sprite.play('jump')
+	body.jump(JUMP_FORCE)
