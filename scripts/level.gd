6578e2e1ccba5da21fc3901b7da9abd58d0ba4ae
Eusébio Alves <eusebio.a95@hotmail.com>
2024-01-08 15:57:56 +0000

    Added next level to load

--- scripts/level.gd
+++ scripts/level.gd
@@ -1,12 +1,16 @@
 extends Node2D
 
+@export var next_level : PackedScene = null 
+
 @onready var start = $Start
 @onready var exit = $Exit
+@onready var deathzone = $Deathzone
 
 var player = null
 
 func _ready():
 	exit.connect('body_entered', _on_body_entered)
+	deathzone.body_entered.connect(_on_deathzone_body_entered)
 	player = get_tree().get_first_node_in_group('player')
 	if player != null:
 		player.position = start.get_spawn_pos()
@@ -32,3 +36,6 @@ func _on_trap_touched_player():
 func _on_body_entered(body):
 	body.active = false
 	exit.animate()
+	await get_tree().create_timer(1.5).timeout
+	if next_level != null:
+		get_tree().change_scene_to_packed(next_level)
