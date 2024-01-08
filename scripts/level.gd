2825ccdbc4f846a3fe6dd73bf40ec79adbd304a9
Eusébio Alves <eusebio.a95@hotmail.com>
2024-01-08 08:51:20 +0000

    Added Marker2D for spawn position

--- scripts/level.gd
+++ scripts/level.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+@onready var start_pos = $StartPosition
+
 func _process(delta):
 	if Input.is_action_pressed('quit'):
 		get_tree().quit()
@@ -8,4 +10,4 @@ func _process(delta):
 
 
 func _on_deathzone_body_entered(body):
-	get_tree().reload_current_scene()
+	body.position = start_pos.position
